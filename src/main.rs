8303fc1702890ac9dafe4bb65de6404fde490c63
uselessgoddess <dodickgod@gmail.com>
2022-08-26 12:04:39 +0300

    Some improvements in project inner

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,67 @@
-use doublets::{mem, unit, Doublets, Links};
-use doublets::DoubletsExt;
+use doublets::{data::LinkType, mem, unit, Doublets, Error};
+use tap::Pipe;
 
-//         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-//         private ulong[] CreateAllVariants2Core(ulong[] sequence, ulong startAt, ulong stopAt)
-//         {
-//             if ((stopAt - startAt) == 0)
-//             {
-//                 return new[] { sequence[startAt] };
-//             }
-//             if ((stopAt - startAt) == 1)
-//             {
-//                 return new[] { Links.Unsync.GetOrCreate(sequence[startAt], sequence[stopAt]) };
-//             }
-//             var variants = new ulong[Platform.Numbers.Math.Catalan(stopAt - startAt)];
-//             var last = 0;
-//             for (var splitter = startAt; splitter < stopAt; splitter++)
-//             {
-//                 var left = CreateAllVariants2Core(sequence, startAt, splitter);
-//                 var right = CreateAllVariants2Core(sequence, splitter + 1, stopAt);
-//                 for (var i = 0; i < left.Length; i++)
-//                 {
-//                     for (var j = 0; j < right.Length; j++)
-//                     {
-//                         var variant = Links.Unsync.GetOrCreate(left[i], right[j]);
-//                         if (variant == Constants.Null)
-//                         {
-//                             throw new NotImplementedException("Creation cancellation is not implemented.");
-//                         }
-//                         variants[last++] = variant;
-//                     }
-//                 }
-//             }
-//             return variants;
-//         }
+#[rustfmt::skip]
+const CATALAN_NUMBERS: [u64; 25] = [
+    1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796, 58786, 208012,
+    742900, 2674440, 9694845, 35357670, 129644790,  477638700, 1767263190,
+    6564120420, 24466267020, 91482563640, 343059613650, 1289904147324,
+];
 
-// catalan numbers
-static CATALAN_NUMBERS: [usize; 25] = [
-    1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796, 58786, 208012, 742900, 2674440, 9694845, 35357670, 129644790, 477638700, 1767263190, 6564120420, 24466267020, 91482563640, 343059613650, 1289904147324];
-
-fn catalan_number(n: usize) -> usize {
+const fn catalan_number(n: usize) -> u64 {
     CATALAN_NUMBERS[n]
 }
 
-fn create_all_sequence_variants<TStore, TLinkAddress>(store: &mut TStore, sequence: &[TLinkAddress], start_at: usize, stop_at: usize) -> Result<Vec<TLinkAddress>, doublets::Error<TLinkAddress>>
+fn spec_all_variants<T, S>(store: &mut S, seq: &[T]) -> Result<Vec<T>, Error<T>>
 where
-    TLinkAddress: doublets::data::LinkType,
-    TStore: Doublets<TLinkAddress>,
+    T: LinkType,
+    S: Doublets<T>,
 {
-    if stop_at - start_at == 0 {
-        return Ok([sequence[start_at]].to_vec());
-    }
-    if stop_at - start_at == 1 {
-        return Ok([store.get_or_create(sequence[start_at], sequence[stop_at])?].to_vec());
-    }
-    let mut variants = Vec::with_capacity(catalan_number(stop_at - start_at) as usize);
-    for splitter in start_at..stop_at {
-        let left = create_all_sequence_variants(store, sequence, start_at, splitter)?;
-        let right = create_all_sequence_variants(store, sequence, splitter + 1, stop_at)?;
-        for i in 0..left.len() {
-            for j in 0..right.len() {
-                let variant = store.get_or_create(left[i], right[j])?;
-                // if variant == TLinkAddress::funty(0) {
-                //     return Err(doublets::Error::CreationCancelled);
-                // }
-                variants.push(variant);
+    assert!(seq.len() > 2);
+
+    let mut variants = Vec::with_capacity(catalan_number(seq.len() - 1) as usize);
+    for splitter in 1..seq.len() {
+        let (left, right) = seq.split_at(splitter);
+        let (left, right) = (
+            all_seq_variants(store, left)?,
+            all_seq_variants(store, right)?,
+        );
+        for from in left {
+            for &to in &right {
+                variants.push(store.get_or_create(from, to)?);
             }
         }
     }
     Ok(variants)
 }
 
-fn main() -> Result<(), doublets::Error<usize>> {
-    // use file as memory for doublets
-    let mem = mem::FileMapped::from_path("db.links")?;
+fn all_seq_variants<T, S>(store: &mut S, seq: &[T]) -> Result<Vec<T>, Error<T>>
+where
+    T: LinkType,
+    S: Doublets<T>,
+{
+    match seq {
+        &[single] => {
+            vec![single]
+        }
+        &[from, to] => {
+            vec![store.get_or_create(from, to)?]
+        }
+        seq => spec_all_variants(store, seq)?,
+    }
+    .pipe(Ok)
+}
+
+fn main() -> Result<(), Error<usize>> {
+    let mem = mem::Global::new();
     let mut store = unit::Store::<usize, _>::new(mem)?;
 
-    // Create a vector of N points using loop
-    let n = 15;
-    let mut sequence = Vec::with_capacity(n);
-    for _ in 0..n {
-        sequence.push(store.create_point()?);
-    }
+    // Create a vector of N points using iterators
+    let seq: Vec<_> = (0..15)
+        .map(|_| store.create_point())
+        .collect::<Result<_, _>>()?;
 
-    create_all_sequence_variants(
-        &mut store,
-        &sequence,
-        0,
-        n - 1
-    )?;
-
-    // `any` constant denotes any link
-    let any = store.constants().any;
-
-    // print all store from store where (index: any, source: any, target: any)
-    store.each_iter([any, any, any]).for_each(|link| {
-        println!("{link:?}");
-    });
+    all_seq_variants(&mut store, &seq)?;
 
     Ok(())
 }
